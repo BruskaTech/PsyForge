b2f65be6d9aa3ad4d5dd8b256080ecc522c9f880
James Bruska <bruskatech@protonmail.com>
2025-05-09 13:44:00 -0400

    Add AsTask for Awaitables

--- Runtime/Scripts/Utilities/Extensions.cs
+++ Runtime/Scripts/Utilities/Extensions.cs
@@ -466,6 +466,16 @@ namespace PsyForge.Extensions {
         }
     }
 
+    public static class AwaitableExtensions {
+        public static async Task AsTask(this Awaitable a) {
+            await a;
+        }
+
+        public static async Task<T> AsTask<T>(this Awaitable<T> a) {
+            return await a;
+        }
+    }
+
     public static class MonoBehaviourExtensions {
         /// <summary>
         /// Quits the game
